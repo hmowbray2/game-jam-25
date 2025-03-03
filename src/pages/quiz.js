0a4c74b63c5213ab57181589ceef372ec0888e91
Hannah Mowbray <hannahmowbray3@gmail.com>
2025-03-03 10:57:06 +0000

    adds page naviagtion
    Signed-off-by: Hannah Mowbray <hannahmowbray3@gmail.com>

--- src/pages/quiz.js
+++ src/pages/quiz.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export const Quiz = () => {
+  const navigate = useNavigate();
   return (
     <>
       <p>Take our quiz!</p>
+      <button onClick={() => navigate("/")}>back</button>
     </>
   );
 };
